ec61cf6306a56d373b7c5910283c512d88d42bbd
khanh <khanh281096@gmail.com>
2018-12-05 22:56:47 +0700

    handle multi language

--- app/src/main/java/com/pt/khanh/movie/screen/detail/castDetail/CastDetailViewModel.java
+++ app/src/main/java/com/pt/khanh/movie/screen/detail/castDetail/CastDetailViewModel.java
@@ -3,9 +3,9 @@ package com.pt.khanh.movie.screen.detail.castDetail;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.databinding.BindingAdapter;
+import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -21,10 +21,10 @@ import com.pt.khanh.movie.utils.StringUtils;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class CastDetailViewModel extends AndroidViewModel {
+    public ObservableBoolean mIsLoading = new ObservableBoolean();
     public ObservableField<Cast> mCastObservableField = new ObservableField<>();
     private MovieRepository mRepository;
     private MovieByAdapter mAdapter;
@@ -56,6 +56,7 @@ public class CastDetailViewModel extends AndroidViewModel {
 
     public void getMoviesByCast(long id) {
         Disposable disposable = mRepository.getMoviesByCast(id, Constants.PAGE_DEFAULT)
+                .doOnSubscribe(disposable1 -> mIsLoading.set(true))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(response -> handleResponse(response),
@@ -65,19 +66,13 @@ public class CastDetailViewModel extends AndroidViewModel {
 
     public void getCastDetail(long id) {
         Disposable disposable = mRepository.getCastDetail(id)
+                .doOnSubscribe(disposable1 -> mIsLoading.set(true))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Cast>() {
-                    @Override
-                    public void accept(Cast cast) throws Exception {
-                        mCastObservableField.set(cast);
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-
-                    }
-                });
+                .subscribe(cast -> {
+                    mIsLoading.set(false);
+                    mCastObservableField.set(cast);
+                }, throwable -> mIsLoading.set(false));
         mCompositeDisposable.add(disposable);
     }
 
@@ -86,10 +81,11 @@ public class CastDetailViewModel extends AndroidViewModel {
     }
 
     private void handleError(Throwable error) {
-
+        mIsLoading.set(false);
     }
 
     private void handleResponse(MovieResult response) {
+        mIsLoading.set(false);
         mAdapter.setMovies(response.getMovies());
     }
 
